17cd378bdb676c8be53ed720d674caaeb94a82db
relmelegy <77593012+relmelegy@users.noreply.github.com>
2024-05-04 04:28:18 -0400

    modified:   package.json 	modified:   src/App.js 	modified:   src/index.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-// import 'index.css';
-// import 'tailwindcss/tailwind.css';
-
 
 const App = () => {
   return(
-  <div>
-    <h1 className="bg-yellow-400 w-screen text-xl font-medium py-4 mx-auto text-center">
-      Notes Keeper
-    </h1>
-    <form className='py-2 shadow-xl rounded-lg px-5 w-1/3 mx-auto text-left mt-10'>
-      <input type="text" />
-      <textarea type="text" />
-    </form>
-  </div>
+    <div>
+      <h1 className="bg-yellow-400 w-screen text-xl font-medium py-4 mx-auto text-center">
+        Notes Keeper
+      </h1>
+      <form className="py-2 shadow-xl rounded-lg px-5 w-1/3 mx-auto text-left mt-10">
+        <input 
+        className= "block shadow w-full mx-auto px-2 py-2 rounded-lg"
+        type="text" 
+        />
+        <textarea 
+        className="block shadow w-full mx-auto my-2 px-2 py-4 rounded-lg"
+        type="text" 
+        />
+      </form>
+    </div>
   );
 };
 
