910c71e82659cc211b3892809a8f6efe0d3f4101
relmelegy <77593012+relmelegy@users.noreply.github.com>
2024-05-04 02:47:29 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/index.css 	new file:   tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return(
+  <div>
+    <h1 className='bg-yellow-400 w-screen text-xl font-medium py-4 mx-auto text-center'>Notes Keeper</h1>
+    <form>
+      <input type='text' />
+      <textarea type="text" />
+    </form>
+  </div>
   );
-}
+};
 
 export default App;
