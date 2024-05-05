e4094ca2b98571cb0a291cfbbd61a73e3b535ee3
relmelegy <77593012+relmelegy@users.noreply.github.com>
2024-05-05 01:08:41 -0400

    update

--- frontend/src/components/note.jsx
+++ frontend/src/components/note.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 const Note = (props) => {
+    const [editMode, setEditMode] = useState(false);
+
     return (
         <div className="border-2 px-5 py-2 w-full my-10">
-            <h1 className="flex justify-center items-center py-2 text-xl font-bold">{props.title}</h1>
-            <h2 className="text-justify">{props.content}</h2>
-            <div className="flex justify-center items-center">
-                <button className="py-1 my-2 px-3 bg-blue-400 text-xl text-white rounded-lg mx-2">
-                    Edit
-                </button>
-                <button className="py-1 my-2 px-3 bg-red-600 text-xl text-white rounded-lg mx-2">
-                    Delete
-                </button>
+            {
+                editMode ? (
+                    <div>in edit mode</div>
+                ) : (
+            <div>
+                <h1 className="flex justify-center items-center py-2 text-xl font-bold">{props.title}</h1>
+                <h2 className="text-justify">{props.content}</h2>
+                <div className="flex justify-center items-center">
+                    <button onClick = {() => setEditMode(true)}
+                    className="py-1 my-2 px-3 bg-blue-400 text-xl text-white rounded-lg mx-2">
+                        Edit
+                    </button>
+                    <button className="py-1 my-2 px-3 bg-red-600 text-xl text-white rounded-lg mx-2">
+                        Delete
+                    </button>
+                </div>
             </div>
+            )}
         </div>
-
+        
     );
 };
 
