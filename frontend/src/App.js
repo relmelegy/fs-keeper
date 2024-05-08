3d627e18f8057ed27e235cc984fca29f1f4195ec
relmelegy <77593012+relmelegy@users.noreply.github.com>
2024-05-08 08:24:31 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -136,7 +136,6 @@
 
 
 
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Note from './components/note';
@@ -144,7 +143,8 @@ import { auth } from './components/firebase';
 import GoogleSignIn from './components/GoogleSignIn';
 import SignOut from './components/SignOut';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const backendUrl = process.env.REACT_APP_BACKEND_URL || 'https://fs-keeper.onrender.com';
+
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [title, setTitle] = useState('');
@@ -162,7 +162,7 @@ const App = () => {
     try {
       if (user) {
         const idToken = await auth.currentUser.getIdToken();
-        const res = await axios.get(`${BACKEND_URL}/notes`, {
+        const res = await axios.get(`${backendUrl}/notes`, {
           headers: {
             Authorization: `Bearer ${idToken}`
           }
@@ -182,9 +182,11 @@ const App = () => {
     e.preventDefault();
     try {
       const idToken = await auth.currentUser.getIdToken();
+      const user = auth.currentUser;
+      const userId = user ? user.uid : null;
       const res = await axios.post(
-        `${BACKEND_URL}/notes`,
-        { title, content },
+        `${backendUrl}/notes`,
+        { title, content, userId },
         { headers: { Authorization: `Bearer ${idToken}` } }
       );
       setNotes([...notes, res.data]);
@@ -198,7 +200,7 @@ const App = () => {
   const deleteNote = async (id) => {
     try {
       const idToken = await auth.currentUser.getIdToken();
-      await axios.delete(`${BACKEND_URL}/notes/${id}`, {
+      await axios.delete(`${backendUrl}/notes/${id}`, {
         headers: { Authorization: `Bearer ${idToken}` }
       });
       setNotes(notes.filter((note) => note._id !== id));
@@ -211,7 +213,7 @@ const App = () => {
     try {
       const idToken = await auth.currentUser.getIdToken();
       const res = await axios.patch(
-        `${BACKEND_URL}/notes/${id}`,
+        `${backendUrl}/notes/${id}`,
         { title: updatedTitle, content: updatedContent },
         { headers: { Authorization: `Bearer ${idToken}` } }
       );
@@ -229,10 +231,7 @@ const App = () => {
       {user ? (
         <div>
           <SignOut />
-          <form
-            onSubmit={addNote}
-            className="py-4 shadow-xl rounded-lg px-8 w-1/3 mx-auto mt-10"
-          >
+          <form onSubmit={addNote} className="py-4 shadow-xl rounded-lg px-8 w-1/3 mx-auto mt-10">
             <input
               value={title}
               onChange={(e) => setTitle(e.target.value)}
@@ -244,6 +243,7 @@ const App = () => {
               value={content}
               onChange={(e) => setContent(e.target.value)}
               className="block shadow w-full mx-auto px-4 py-4 rounded-lg mb-4"
+              type="text"
               placeholder="Note Content"
             />
             <button
@@ -254,19 +254,17 @@ const App = () => {
             </button>
           </form>
           {notes && notes.length > 0 && (
-            <div className="flex justify-center px-8 mt-8">
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 w-full">
-                {notes.map((note) => (
-                  <Note
-                    key={note._id}
-                    id={note._id}
-                    title={note.title}
-                    content={note.content}
-                    delete={() => deleteNote(note._id)}
-                    updateNote={updateNote}
-                  />
-                ))}
-              </div>
+            <div className="grid grid-cols-4 gap-4 py-2">
+              {notes.map((note) => (
+                <Note
+                  key={note._id}
+                  id={note._id}
+                  title={note.title}
+                  content={note.content}
+                  delete={() => deleteNote(note._id)}
+                  updateNote={updateNote}
+                />
+              ))}
             </div>
           )}
         </div>
@@ -278,3 +276,4 @@ const App = () => {
 };
 
 export default App;
+
