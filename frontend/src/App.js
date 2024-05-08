748c3df96e64395a017a972180ba25232d4192ff
relmelegy <77593012+relmelegy@users.noreply.github.com>
2024-05-08 07:24:39 -0400

    Update backend URL to deployed instance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -144,8 +144,7 @@ import { auth } from './components/firebase';
 import GoogleSignIn from './components/GoogleSignIn';
 import SignOut from './components/SignOut';
 
-const BACKEND_URL = 'https://fs-keeper.onrender.com';
-
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [title, setTitle] = useState('');
